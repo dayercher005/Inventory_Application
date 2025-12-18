5d209a6ff47c79b325cc103f77d0cb91f213858d
Dayer <dayerxr.cher@gmail.com>
2025-12-18 11:45:48 +0800

    Initialising database node-postgres code

---
+++ src/db/seed.js
@@ -0,0 +1,16 @@
+import pool from '/db/pool.js'
+import Client from 'pg'
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon');
+`;
+
