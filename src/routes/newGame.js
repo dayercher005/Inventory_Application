8a3c131d1296d3ccc132838b8de3f72d7608055f
Dayer <dayerxr.cher@gmail.com>
2025-12-23 15:32:59 +0800

    Debugging new routes

--- src/routes/newGame.js
+++ src/routes/newGame.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { renderNewGameNavbar } from '../controllers/navbar.js';
-import { renderNewGameForm } from '../controllers/newGame.js'
+import { renderNewGameForm } from '../controllers/newGame.js';
+import { sendNewGameForm } from '../controllers/newGame.js';
 
 export const newGameRouter = Router();
 
 newGameRouter.get("/", renderNewGameNavbar);
 newGameRouter.get("/", renderNewGameForm);
+newGameRouter.post("/", sendNewGameForm);
