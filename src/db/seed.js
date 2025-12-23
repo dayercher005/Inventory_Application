91c05224114bc9af5242e947cbe83b31f7fca63f
Dayer <dayerxr.cher@gmail.com>
2025-12-23 12:46:35 +0800

    Configuring environment variables and setting up database via script

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,16 +1,47 @@
-import pool from '/db/pool.js'
-import Client from 'pg'
+#! /usr/bin/env node
+
+import { Client } from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS usernames (
+CREATE TABLE IF NOT EXISTS games (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR ( 255 )
+  name VARCHAR ( 255 ),
+  price DECIMAL ( 2 ),
+  categories VARCHAR ( 255 )
 );
 
-INSERT INTO usernames (username) 
+INSERT INTO games (name, price, categories) 
 VALUES
-  ('Bryan'),
-  ('Odin'),
-  ('Damon');
+  ('Counter Strike 2', '0.00', 'Shooter'),
+  ('Dota 2', '0.00', 'MOBA'),
+  ('Arc Raiders', '54.00', 'Shooter'),
+  ('Bongo Cat', '0.00', 'Idler'),
+  ('Path of Exile 2', '40.44', 'RPG'),
+  ('Marvel Rivals', '0.00', 'Shooter'),
+  ('PUBG: BATTLEGROUNDS', '0.00', 'Shooter'),
+  ('Warframe', '0.00', 'Shooter'),
+  ('Rust', '20.00', 'Survival'),
+  ('Tomb Raider', '2.39', 'Survival'),
+  ('Shell Diver', '3.93', 'Idler'),
+  ('MageQuit', '5.80', 'MOBA'),
+  ('Hades', '6.60', 'RPG')
 `;
+
+
+async function main(){
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DATABASE
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
 
