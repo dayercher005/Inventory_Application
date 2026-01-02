3fad62deb4c816aa4f54711395b0cc4e63e56c01
Dayer <dayerxr.cher@gmail.com>
2026-01-02 11:48:46 +0800

    refactor: refactored routes and removed updateGame router

--- src/routes/AllGames/allGames.js
+++ src/routes/AllGames/allGames.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { renderAllGames, renderIndividualGames } from '../../controllers/AllGames/allGames.js';
+import { renderUpdatedGameForm, sendUpdatedGameForm} from '../../controllers/AllGames/updateGame.js'
 import { renderAllGamesNavbar } from '../../controllers/navbar.js';
 
 export const allGamesRouter = Router();
@@ -7,3 +8,5 @@ export const allGamesRouter = Router();
 allGamesRouter.get("/", renderAllGamesNavbar);
 allGamesRouter.get("/", renderAllGames);
 allGamesRouter.get("/:gameID", renderIndividualGames);
+allGamesRouter.get("/:gameID/updateGame", renderUpdatedGameForm);
+allGamesRouter.get("/:gameID/updateGame", sendUpdatedGameForm);
