d7ea4051eb644045af863153cadb642b32a78b99
Dayer <dayerxr.cher@gmail.com>
2026-01-02 16:55:28 +0800

    feat: configured routes for deleting of games

--- src/routes/AllGames/allGames.js
+++ src/routes/AllGames/allGames.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { renderAllGames, renderIndividualGames } from '../../controllers/AllGames/allGames.js';
-import { renderUpdatedGameForm, sendUpdatedGameForm} from '../../controllers/AllGames/updateGame.js'
+import { renderUpdatedGameForm, sendUpdatedGameForm} from '../../controllers/AllGames/updateGame.js';
+import { DeleteGame } from '../../controllers/AllGames/deleteGame.js';
 import { renderAllGamesNavbar } from '../../controllers/navbar.js';
 
 export const allGamesRouter = Router();
@@ -9,4 +10,5 @@ allGamesRouter.get("/", renderAllGamesNavbar);
 allGamesRouter.get("/", renderAllGames);
 allGamesRouter.get("/:gameID", renderIndividualGames);
 allGamesRouter.get("/:gameID/updateGame", renderUpdatedGameForm);
+allGamesRouter.post("/:gameID", DeleteGame);
 allGamesRouter.post("/:gameID/updateGame", sendUpdatedGameForm);
